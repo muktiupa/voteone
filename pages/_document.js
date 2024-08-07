919569240c18663b9ec3123d20f566be1c2583eb
Mukti Upadhyay <100656884+muktiupa@users.noreply.github.com>
2024-08-07 17:54:20 +0530

    screen size fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div style={{width: "600px"}} className=" flex justify-evenly  mx-auto">
+        <div style={{height:"100vh"}} className=" flex justify-evenly  mx-auto">
           <Main />
         </div>
         <NextScript />

f329f81b0f1e1c44f14f331005cdcf6c87cb04a5
Mukti Upadhyay <100656884+muktiupa@users.noreply.github.com>
2024-08-08 11:05:34 +0530

    some last time changes
    mukti

--- components/ui/NeoHeader.jsx
+++ components/ui/NeoHeader.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const NeoHeader = () => {
     return (
-      <div style={{ background: 'rgb(226 232 240)' }} className="h-[30vh] w-full rounded-b-[45px] m-auto">
+      <div style={{ background: 'rgb(226 232 240)' }} className="h-[25vh] w-full rounded-b-[45px] m-auto">
         <div className="flex items-center justify-between w-full pt-4 p-2 p-x-9">
           <Image src="/ggt_logo.png" alt="Logo" width={80} height={80} className="w-20" />
           <Image src="/gail.png" alt="Logo" width={350} height={100} className="w-[35%]" />
